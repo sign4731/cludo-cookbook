852124b5ebd8e007378f9492b7709add77e0bc3f
AndyPcludo <69457361+AndyPcludo@users.noreply.github.com>
2022-02-02 00:04:56 +0100

    Adjust CludoInputs component, minor updates

--- src/components/CludoInputs.jsx
+++ src/components/CludoInputs.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 class CludoInputs extends React.Component {
     render() {
         return (
-            <div className="cludo-search-input__input-form">
-                <input name="searchrequest" id="cludo-search-form-input"
-                    type="search" placeholder="Chercher"
-                    title="Tapez le terme de recherche" autoComplete="on"
-                    aria-autocomplete="list" aria-haspopup="true" class={this.props.class}/>
-                <button type="button" title="Chercher">Chercher</button>
+            <div>
+                <h4>{this.props.title}</h4>
+                <div className="cludo-search-input__input-form">
+                    <input name="searchrequest" id="cludo-search-form-input"
+                        type="search" placeholder="Chercher"
+                        title="Tapez le terme de recherche" autoComplete="on"
+                        aria-autocomplete="list" aria-haspopup="true" class={this.props.class}/>
+                    <button type="button" title="Chercher">Chercher</button>
+                </div>
             </div>
         );
     }
--- src/components/SearchResultInput.jsx
+++ src/components/SearchResultInput.jsx
@@ -1,10 +1,12 @@
-import Prism from 'prismjs';
-import "prismjs/themes/prism-okaidia.css";
 import CodeBlock from 'react-copy-code';
 import Code from '../components/Code';
 import CludoInputs from '../components/CludoInputs';
+import Prism from 'prismjs';
+import "prismjs/themes/prism-okaidia.css";
 
 export default function SearchResultInput() {
+    setTimeout(() => Prism.highlightAll(), 0); //highlight the code after it is render in the DOM
+
     const plainRectangle = 
     `<div class="cludo-search-input__input-form">
     <input name="searchrequest" id="cludo-search-form-input"
@@ -15,8 +17,8 @@ export default function SearchResultInput() {
 </div>`;
 
     const plainBorderRadius =
-        `<div className="cludo-search-input__input-form">
-    <input name="searchrequest" className="cludo-radius-input" id="cludo-search-form-input"
+        `<div class="cludo-search-input__input-form">
+    <input name="searchrequest" class="cludo-radius-input" id="cludo-search-form-input"
         type="search" placeholder="Chercher" 
         title="Tapez le terme de recherche" autoComplete="on"
         aria-autocomplete="list" aria-haspopup="true" />
@@ -24,8 +26,8 @@ export default function SearchResultInput() {
 </div>`;
 
     const line =
-        `<div className="cludo-search-input__input-form">
-    <input name="searchrequest" className="cludo-line-input" id="cludo-search-form-input"
+        `<div class="cludo-search-input__input-form">
+    <input name="searchrequest" class="cludo-line-input" id="cludo-search-form-input"
         type="search" placeholder="Chercher" 
         title="Tapez le terme de recherche" autoComplete="on"
         aria-autocomplete="list" aria-haspopup="true" />
@@ -37,17 +39,14 @@ export default function SearchResultInput() {
             <div className="component-container">
                 <h3>Search result input:</h3>
                 
-                <h4>Plain Rectangle:</h4>
-                <CludoInputs />
+                <CludoInputs title="Plain Rectangle"/>
                 <Code inputType={plainRectangle}/>
 
-                <h4>Plain Border radius:</h4>
-                <CludoInputs class="cludo-radius-input"/>
+                <CludoInputs title="Plain Border Radius" class="cludo-radius-input"/>
                 <Code inputType={plainBorderRadius}/>
 
-                <h4>Line</h4>
-                <CludoInputs class="cludo-line-input"/>
-                <Code inputType={line} />
+                <CludoInputs title="Line" class="cludo-line-input"/>
+                <Code inputType={line}/>
             </div>
         </CodeBlock>
     );
